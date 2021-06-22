c0ca289ec113589439447b391839e81320e90d1f
psalvi <salvipablo@gmail.com>
2021-06-22 11:28:28 -0300

    Finalizacion de estilos y campos en alta
    Se agregaron los campos que faltaban
    se cambiaron los estilos, para que
    se vea habilitado o no
    falta finalizar la carga de datos
    
    Resolve: T#03

--- js/create.js
+++ js/create.js
@@ -1,14 +1,66 @@
 "use strict";
 
 const statusBar = document.getElementById("statusBar-text");
-
 const dateSupInput = document.getElementById("dateSup");
 const clientInput = document.getElementById("client");
 const serialNumberInput = document.getElementById("serialNumber");
 const supportNumberInput = document.getElementById("supportNumber");
-
+const orderNumber = document.getElementById("orderNumber");
+const typeOfJob = document.getElementById("typeOfJob");
+const observations = document.getElementById("observations");
+const finalized = document.getElementById("finalized");
 
 const IDBRequest = indexedDB.open("soportes", 1);
+
+function activeNoActive (active) {
+    if (active) {
+        dateSupInput.classList.replace("field-frame-noActive", "field-frame-active");
+        clientInput.classList.replace("field-frame-noActive", "field-frame-active");
+        serialNumberInput.classList.replace("field-frame-noActive", "field-frame-active");
+        supportNumberInput.classList.replace("field-frame-noActive", "field-frame-active");
+        orderNumber.classList.replace("field-frame-noActive", "field-frame-active");
+        typeOfJob.classList.replace("field-frame-noActive", "field-frame-active");
+        observations.classList.replace("field-frame-noActive", "field-frame-active");
+        finalized.classList.replace("field-frame-noActive", "field-frame-active");
+
+        dateSupInput.disabled = false;
+        clientInput.disabled = false;
+        serialNumberInput.disabled = false;
+        supportNumberInput.disabled = false;
+        orderNumber.disabled = false;
+        typeOfJob.disabled = false;
+        observations.disabled = false;
+        finalized.disabled = false;
+    } else {
+        dateSupInput.classList.replace("field-frame-active", "field-frame-noActive");
+        clientInput.classList.replace("field-frame-active", "field-frame-noActive");
+        serialNumberInput.classList.replace("field-frame-active", "field-frame-noActive");
+        supportNumberInput.classList.replace("field-frame-active", "field-frame-noActive");
+        orderNumber.classList.replace("field-frame-active", "field-frame-noActive");
+        typeOfJob.classList.replace("field-frame-active", "field-frame-noActive");
+        observations.classList.replace("field-frame-active", "field-frame-noActive");
+        finalized.classList.replace("field-frame-active", "field-frame-noActive");
+
+        dateSupInput.disabled = true;
+        clientInput.disabled = true;
+        serialNumberInput.disabled = true;
+        supportNumberInput.disabled = true;
+        orderNumber.disabled = true;
+        typeOfJob.disabled = true;
+        observations.disabled = true;
+        finalized.disabled = true;
+
+        dateSupInput.value = "";
+        clientInput.value = "";
+        serialNumberInput.value = "";
+        supportNumberInput.value = "";
+        orderNumber.value = "";
+        typeOfJob.value = "";
+        observations.value = "";
+        finalized.value = "";
+        }
+}
+
 
 IDBRequest.addEventListener("upgradeneeded", () => {
     const db = IDBRequest.result;
@@ -26,18 +78,22 @@ IDBRequest.addEventListener("error", () => {
 });
 
 document.getElementById("btnNew").addEventListener("click", () => {
-    dateSupInput.disabled = false;
-    clientInput.disabled = false;
-    serialNumberInput.disabled = false;
-    supportNumberInput.disabled = false;
+
+    activeNoActive(true);
+
     statusBar.innerHTML = "Cargando nuevo registro";
 });
 
 document.getElementById("btnCreate").addEventListener("click", () => {
-    let dateSup = document.getElementById("dateSup").value;
-    let client = document.getElementById("client").value;
-    let serialNumber = document.getElementById("serialNumber").value;
-    let supportNumber = document.getElementById("supportNumber").value;
+    let dateSup = dateSupInput.value;
+    let client = clientInput.value;
+    let serialNumber = serialNumberInput.value;
+    let supportNumber = supportNumberInput.value;
+
+    //let dateSup = document.getElementById("dateSup").value;
+    //let client = document.getElementById("client").value;
+    //let serialNumber = document.getElementById("serialNumber").value;
+    //let supportNumber = document.getElementById("supportNumber").value;
 
     if (dateSup.length > 0) {
         if (document.querySelector(".posible") != undefined) {
@@ -59,15 +115,8 @@ document.getElementById("btnCreate").addEventListener("click", () => {
             });
             statusBar.innerHTML = "Registro agregado correctamente";
         }
-        dateSupInput.disabled = true;
-        clientInput.disabled = true;
-        serialNumberInput.disabled = true;
-        supportNumberInput.disabled = true;
     }
-    document.getElementById("dateSup").value = "";
-    document.getElementById("client").value = "";
-    document.getElementById("serialNumber").value = "";
-    document.getElementById("supportNumber").value = "";
+    activeNoActive(false);
 });
 
 const addObjeto = objeto => {
